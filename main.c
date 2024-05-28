04a0e35d742e2d6c5682fa7fcdc0bb45e025430f
root <kennykguo@gmail.com>
2024-05-28 17:22:11 +0000

    update

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <termios.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 void enable_raw_mode(void) {
     struct termios termios;
@@ -8,10 +10,22 @@ void enable_raw_mode(void) {
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &termios);
 }
 
+#define MAX_INPUT_SIZE 4096
+#define MAX_ARGS 128
+
 int main(void) {
+    // Raw mode is a mode where the terminal does not perform any processing or handling of the input and output.
     enable_raw_mode();
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    int index = 0;
+    // Reads from the std input. If function returns 1, then the character was successfully read.
+    // read will constantly read a character
+    while (true){
+        if (read(STDIN_FILENO, &c, 1) != 1 || c == ' ' || c == '\t' || c == '\n' || c == 'q') {
+            
+        }
+    }
+
     return 0;
 }
 
